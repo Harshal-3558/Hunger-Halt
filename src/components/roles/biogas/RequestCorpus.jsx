7673181ceea042574fe5630d14b4c86e40808ee0
Harshal Patil <patilharshal3558@outlook.com>
2024-10-29 21:01:55 +0530

    Changes in Biogas Homepage

--- src/components/roles/biogas/RequestCorpus.jsx
+++ src/components/roles/biogas/RequestCorpus.jsx
@@ -1,37 +1,33 @@
-// import { useState, useEffect, useRef } from "react";
-// import {
-//   Button,
-//   Modal,
-//   ModalOverlay,
-//   ModalContent,
-//   ModalHeader,
-//   ModalFooter,
-//   ModalBody,
-//   ModalCloseButton,
-//   useDisclosure,
-//   FormControl,
-//   FormLabel,
-//   Input,
-// } from "@chakra-ui/react";
 import { FaCirclePlus } from "react-icons/fa6";
 import { FiCheckCircle } from "react-icons/fi";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useState } from "react";
-// import SubmitButton from "./SubmitButton";
-// import { handleGetLocation, initializeMap } from "../../../map/getLocation";
-// import WorkingLocation from "../../auth/WorkingLocation";
+import { useToast } from "@chakra-ui/react";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 export default function RequestCorpus() {
   const [requested,setRequested] = useState(false)
+  const toast = useToast()
+
+  const handleRequest = () => {
+    setRequested((prev)=>!prev)
+    toast({
+      position: "top",
+      title: requested ? 'Request Removed' : 'Request Added',
+      description: requested ? "Your corpus request has been removed" : "Your corpus request has been added",
+      status: requested ? 'info' : 'success',
+      duration: 3000,
+      isClosable: true,
+    })
+  }
 
   return (
     <div>
       <button
         className={`h-32 md:h-full w-full md:w-full border rounded-xl flex flex-col justify-center items-center space-y-3 transition duration-300 ease-in-out ${requested ? 'bg-green-500' : 'bg-slate-200'}`}
-        onClick={() => setRequested((prev)=>!prev)}
+        onClick={handleRequest}
       >
         <div>
           {requested ? (
